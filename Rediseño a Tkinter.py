783a71e4c22e9ae78c4dac22dfeb38d15d30a8ee
Joseph1103 <josephcoronado11@estudianTEC.cr>
2021-06-17 00:46:28 -0600

    Se agregan la pantalla de creditos y el boton para salir del juego estos totalmente funcionales.

--- Rediseño a Tkinter.py
+++ Rediseño a Tkinter.py
@@ -30,16 +30,16 @@ class Ventana():
         self.boton_start = Button(self.canvas, text="Nivel 1", font=("Airstrike", 25), command=self.primer_nivel)
         self.boton_start.place(x=75, y=300)
     # Boton Nivel 2
-        self.boton_start = Button(self.canvas, text="Nivel 2", font=("Airstrike", 25))  # command=self.first_level)
+        self.boton_start = Button(self.canvas, text="Nivel 2", font=("Airstrike", 25) , command=self.segundo_nivel)
         self.boton_start.place(x=335, y=300)
     # Boton Nivel 3
-        self.boton_start = Button(self.canvas, text="Nivel 3", font=("Airstrike", 25))  # command=self.first_level)
+        self.boton_start = Button(self.canvas, text="Nivel 3", font=("Airstrike", 25), command=self.tercer_nivel)
         self.boton_start.place(x=580, y=300)
     # Boton pagina de creditos
-        self.boton_credits = Button(self.canvas, text="Creditos", font=("Airstrike", 25))# command=self.credits)
+        self.boton_credits = Button(self.canvas, text="Creditos", font=("Airstrike", 25), command=self.creditos)
         self.boton_credits.place(x=20, y=480)
     # Boton Quitar juego
-        self.boton_quitButton = Button(self.canvas, text="Salir", font=("Airstrike", 25))#  command=self.quitGame)
+        self.boton_quitButton = Button(self.canvas, text="Salir", font=("Airstrike", 25), command=self.quitar_juego)
         self.boton_quitButton.place(x=650, y=480)
         self.ventana.mainloop()
 
@@ -49,12 +49,49 @@ class Ventana():
         self.canvas.place(x=-5, y=0)
         bg = PhotoImage(file="space.png")
         self.canvas.create_image(0, 0, image=bg, anchor="nw")
-        self.boton2 = Button(self.canvas, text="Menu", font=("Airstrike", 10), bg="Yellow",command=self.PantallaPrincipal)
+        self.boton2 = Button(self.canvas, text="Menu", font=("Airstrike", 15), bg="Yellow",command=self.PantallaPrincipal)
         self.boton2.place(x=0, y=0)
         self.ventana.mainloop()
 
+    # Nivel 2
+    def segundo_nivel(self):
+        self.canvas = Canvas(self.ventana, width=800, height=600, highlightbackground="White")
+        self.canvas.place(x=-5, y=0)
+        bg = PhotoImage(file="space.png")
+        self.canvas.create_image(0, 0, image=bg, anchor="nw")
+        self.boton2 = Button(self.canvas, text="Menu", font=("Airstrike", 15), bg="Yellow",command=self.PantallaPrincipal)
+        self.boton2.place(x=0, y=0)
+        self.ventana.mainloop()
 
+    # Nivel 3
+    def tercer_nivel(self):
+        self.canvas = Canvas(self.ventana, width=800, height=600, highlightbackground="White")
+        self.canvas.place(x=-5, y=0)
+        bg = PhotoImage(file="space.png")
+        self.canvas.create_image(0, 0, image=bg, anchor="nw")
+        self.boton2 = Button(self.canvas, text="Menu", font=("Airstrike", 15), bg="Yellow", command=self.PantallaPrincipal)
+        self.boton2.place(x=0, y=0)
+        self.ventana.mainloop()
 
+#########################################################################################################################################
+# Creditos
+    def creditos(self):
+        self.canvas = Canvas(self.ventana, width=800, height=600, highlightbackground="White")
+        bg = PhotoImage(file="space.png")
+        self.canvas.create_image(0, 0, image=bg, anchor="nw")
+        self.canvas.place(x=-5, y=0)
+        self.canvas.create_text(400, 260, text="   Creado en Costa Rica\n\n   Tecnologico De Costa Rica \n\n   Ingenieria en Computadores \n\n   2021 Grupo 03 \n\n    Leonardo Araya \n\n    Ver.1.0 \n\n    Joseph Coronado Alvarado \n\n    Alejandro Benavides Juarez \n\n    Tkinter ", font=("Airstrike", 25), fill="green")
+        self.boton2 = Button(self.canvas, text="Menu", font=("Airstrike", 15), bg="Yellow", command=self.PantallaPrincipal)
+        self.boton2.place(x=0, y=0)
+        self.ventana.mainloop()
+
+######################################################################################################################################
+# Quitar juego
+    def quitar_juego(self):
+        self.ventana.destroy()
+        self.ventana.mainloop()
+
+# Fin del programa
 ventana = Tk()
 ventana.title("Proyecto Taller")
 ventana.minsize(800, 600)
